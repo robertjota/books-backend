187cb4d002c6f66dcf4cc96e6dc1dd14fcf3f9ad
Robert Arias <robertjota@gmail.com>
2022-11-02 20:52:43 -0400

    actualizacion curso2

---
+++ tests/Feature/BooksApiTest.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\Book;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class BooksApiTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /** @test */
+    public function can_get_all_books()
+    {
+        $books = Book::factory(4)->create();
+
+        $this->getJson(route('books.index'))
+            ->assertJsonFragment([
+                'title' => $books[0]->title
+            ]);
+    }
+
+    /** @test */
+    public function can_get_one_book()
+    {
+        $book = Book::factory()->create();
+
+        $this->getJson(route('books.show', $book))
+            ->assertJsonFragment([
+                'title' => $book->title
+            ]);
+    }
+
+    /** @test */
+    public function can_create_books()
+    {
+        $this->postJson(route('books.store'), [])
+            ->assertJsonValidationErrorFor('title');
+
+        $this->postJson(route('books.store'), [
+            'title' => 'My new book'
+        ])->assertJsonFragment([
+            'title' => 'My new book'
+        ]);
+
+        $this->assertDatabaseHas('books', [
+            'title' => 'My new book'
+        ]);
+    }
+
+    /** @test */
+    public function can_update_books()
+    {
+        $book = Book::factory()->create();
+
+        $this->patchJson(route('books.update', $book), [])
+            ->assertJsonValidationErrorFor('title');
+
+        $this->patchJson(route('books.update', $book), [
+            'title' => 'Edited book'
+        ])->assertJsonFragment([
+            'title' => 'Edited book'
+        ]);
+
+        $this->assertDatabaseHas('books', [
+            'title' => 'Edited book'
+        ]);
+    }
+
+    /** @test */
+    public function can_delete_books()
+    {
+        $book = Book::factory()->create();
+
+        $this->deleteJson(route('books.destroy', $book))
+            ->assertNoContent();
+
+        $this->assertDatabaseCount('books', 0);
+    }
+}
